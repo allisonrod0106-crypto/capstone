b8fab6d9b08fbf6b762bb95fb7ace50ea4ce1fb8
Allison Rodriguez <allisonrod0106@gmail.com>
2026-04-30 23:28:25 +0200

    add home view with links to register and log in

--- journal-frontend/src/views/Home.vue
+++ journal-frontend/src/views/Home.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Home page</div>
+  <div>
+    <h1>Daily Journal</h1>
+    <p>Build a journaling habit with daily writing prompts.</p>
+
+    <RouterLink to="/register">
+      <button>Get Started</button>
+    </RouterLink>
+
+    <RouterLink to="/login">
+      <button>Log In</button>
+    </RouterLink>
+  </div>
 </template>
