a0742993087298745866629c7f2af95f50c418ca
Allison Rodriguez <allisonrod0106@gmail.com>
2026-04-30 16:48:23 +0200

    add nav guards for protexted roots

--- journal-frontend/src/router/index.js
+++ journal-frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/auth'
 import Home from '../views/Home.vue'
 import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
@@ -16,6 +17,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to) => {
+  const auth = useAuthStore()
+  if (to.meta.requiresAuth && !auth.isAuthenticated) {
+    return '/login'
+  }
 })
 
 export default router
