9ee991101158e461a0a7937d794da53fdcda5ea2
Allison Rodriguez <allisonrod0106@gmail.com>
2026-04-30 16:30:42 +0200

    add Pinia auth store and axios instance with token interceptor

---
+++ journal-frontend/src/stores/auth.js
@@ -0,0 +1,38 @@
+import { defineStore } from 'pinia'
+import api from '../api/axios'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    token: localStorage.getItem('token') || null,
+    user: JSON.parse(localStorage.getItem('user')) || null,
+  }),
+
+  getters: {
+    isAuthenticated: (state) => !!state.token,
+  },
+
+  actions: {
+    async register(data) {
+      const response = await api.post('/api/users/register', data)
+      this.token = response.data.token
+      this.user = response.data.user
+      localStorage.setItem('token', this.token)
+      localStorage.setItem('user', JSON.stringify(this.user))
+    },
+
+    async login(data) {
+      const response = await api.post('/api/users/login', data)
+      this.token = response.data.token
+      this.user = response.data.user
+      localStorage.setItem('token', this.token)
+      localStorage.setItem('user', JSON.stringify(this.user))
+    },
+
+    logout() {
+      this.token = null
+      this.user = null
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+    },
+  },
+})
