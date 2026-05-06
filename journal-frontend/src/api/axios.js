d33659f30b4e04631f1e5a4ba69885a33853cd31
Allison Rodriguez <allisonrod0106@gmail.com>
2026-05-06 09:39:52 +0200

    configure VITE_AP I_URL env variable

--- journal-frontend/src/api/axios.js
+++ journal-frontend/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 })
 
-// Automatically attach the token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
